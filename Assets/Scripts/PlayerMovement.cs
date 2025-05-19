eef2afeb3c0cc02879105f57760f9aa3c9b730b7
Rostyslav2176 <tarasiukrostyslav120@gmail.com>
2025-05-19 13:01:08 +0100

    Slope movement
    Added slope movement
    Fixed player sliding
    Turned off down force for crouch mid air

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,10 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement")] 
     float _speed;
     public float currentSpeed;
+    public float walkSpeed;
+    public float airMultiplier;
     float _groundDrag;
     public float currentDrag;
-    public float maxDrag;
     public float jumpForce;
     public float crouchSpeed;
     public float crouchYScale;
@@ -16,12 +17,17 @@ public class PlayerMovement : MonoBehaviour
     [Header("Key Binds")] 
     public KeyCode jumpKey = KeyCode.Space;
     public KeyCode crouchKey = KeyCode.LeftControl;
+    public KeyCode walkKey = KeyCode.LeftShift;
 
     [Header("Ground Check")] 
-    public float playerHeight = 2f;
     float _groundCheckDistance;
     float _bufferCheckDistance = 0.1f;
     bool _onGround = false;
+    
+    [Header("On Slope Check")] 
+    public float maxSlopeAngle;
+    public RaycastHit SlopeHit;
+    private float _playerHeight = 2.0f;
 
     public Transform orientation;
 
@@ -50,6 +56,7 @@ public class PlayerMovement : MonoBehaviour
         _rb.linearDamping = _onGround ? _groundDrag : 0f;
         
         PlayerInput();
+        SpeedControl();
 
         // Jump
         if (Input.GetKeyDown(jumpKey) && _onGround)
@@ -68,10 +75,16 @@ public class PlayerMovement : MonoBehaviour
         }
         
         //Crouch
-        if (Input.GetKeyDown(crouchKey))
+        if (Input.GetKeyDown(crouchKey) && _onGround) //On ground
         {
             transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
             _rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+            _speed = crouchSpeed;
+        }
+        
+        if (Input.GetKeyDown(crouchKey) && !_onGround) //In air
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
             _speed = crouchSpeed;
         }
 
@@ -82,7 +95,16 @@ public class PlayerMovement : MonoBehaviour
             
         }
         
-        SpeedControl();
+        //Walk
+        if (Input.GetKeyDown(walkKey))
+        {
+            _speed = walkSpeed;
+        }
+
+        if (Input.GetKeyUp(walkKey))
+        {
+            _speed = currentSpeed;
+        }
     }
 
     void FixedUpdate()
@@ -92,24 +114,29 @@ public class PlayerMovement : MonoBehaviour
 
     private void PlayerInput()
     {
-        _horizontalInput = Input.GetAxis("Horizontal");
-        _verticalInput = Input.GetAxis("Vertical");
-        
-        if (_horizontalInput == 0 && _verticalInput == 0)
-        {
-            _groundDrag = maxDrag;
-        }
-        else
-        {
-            _groundDrag = currentDrag;
-        }
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
     }
 
     private void MovePlayer()
     {
         // Calculate move direction
         _moveDirection = orientation.forward * _verticalInput + orientation.right * _horizontalInput;
-        _rb.AddForce(_moveDirection.normalized * _speed * 10f, ForceMode.Force);
+        
+        //On ground
+        if (_onGround)
+        {
+            _rb.AddForce(_moveDirection.normalized * _speed * 10f, ForceMode.Force);
+        }
+        
+        //In air
+        if (!_onGround)
+        {
+            _rb.AddForce(_moveDirection.normalized * _speed * 10f * airMultiplier, ForceMode.Force); 
+        }
+        
+        //On slope turn off gravity
+        _rb.useGravity = !OnSlope();
     }
 
     private void SpeedControl()
@@ -130,5 +157,16 @@ public class PlayerMovement : MonoBehaviour
 
         // Apply upward force
         _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out SlopeHit, _playerHeight * 0.5f + 0.3f))
+        {
+           float angle = Vector3.Angle(SlopeHit.normal, Vector3.up);
+           return angle < maxSlopeAngle && angle != 0;
+        }
+        
+        return false;
     }
 }
